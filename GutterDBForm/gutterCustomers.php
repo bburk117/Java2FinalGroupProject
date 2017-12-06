a75c9e67ca2a5e6abf831790eba1defdf25509dc
bburk117 <31907447+bburk117@users.noreply.github.com>
2017-12-06 07:54:13 -0600

    Add files via upload
    Added Two Webpages, each webpage has a form that submits to the nationalgutter database table. The gutterCustomersForm submits to the customers table, the gutterJobsForm submits to the jobs table.

---
+++ GutterDBForm/gutterCustomers.php
@@ -0,0 +1,46 @@
+<?php
+
+ $firstname = filter_input(INPUT_POST,'fname');
+ $lastname = filter_input(INPUT_POST,'lname');
+ $address = filter_input(INPUT_POST,'address');
+ $city = filter_input(INPUT_POST,'city');
+ $state = filter_input(INPUT_POST,'state');
+ $zip = filter_input(INPUT_POST,'zip');
+ $email = filter_input(INPUT_POST,'email');
+ $phone = filter_input(INPUT_POST,'phone');
+ if (!empty($firstname)){
+if (!empty($lastname)){
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "nationalgutter";
+
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+if (mysqli_connect_error()){
+	die('Connect Error ('. mysqli_connect_errno() .') '
+	 . mysqli_connect_error());
+}
+else{
+	$sql = "INSERT INTO customers (fname, lname, address, city, state, zip, email, phone)
+	values ('$firstname','$lastname','$address','$city','$state','$zip','$email','$phone')";
+	if ($conn->query($sql)){
+		echo "New record is inserted successfully";
+	}
+	else{
+		echo "Error: ". $sql ."<br>". $conn->error;
+	}
+	$conn->close();
+}
+} 
+else{
+	echo "Last name should not be empty";
+	die();
+}
+ }
+ else{
+	 echo "First name should not be empty";
+	 die();
+ }
+?>
---
+++ GutterDBForm/gutterJobs.php
@@ -0,0 +1,47 @@
+<?php
+
+ $customerid = filter_input(INPUT_POST,'custid');
+ $address = filter_input(INPUT_POST,'address');
+ $city = filter_input(INPUT_POST,'city');
+ $state = filter_input(INPUT_POST,'state');
+ $zip = filter_input(INPUT_POST,'zip');
+ $type = filter_input(INPUT_POST,'type');
+ $notes = filter_input(INPUT_POST,'notes');
+ $cost = filter_input(INPUT_POST,'cost');
+ $date = filter_input(INPUT_POST,'date');
+ if (!empty($customerid)){
+if (!empty($address)){
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "nationalgutter";
+
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+if (mysqli_connect_error()){
+	die('Connect Error ('. mysqli_connect_errno() .') '
+	 . mysqli_connect_error());
+}
+else{
+	$sql = "INSERT INTO jobs (custid, address, city, state, zip, type, notes, cost, date)
+	values ('$customerid','$address','$city','$state','$zip','$type','$notes','$cost','$date')";
+	if ($conn->query($sql)){
+		echo "New record is inserted successfully";
+	}
+	else{
+		echo "Error: ". $sql ."<br>". $conn->error;
+	}
+	$conn->close();
+}
+} 
+else{
+	echo "Address should not be empty";
+	die();
+}
+ }
+ else{
+	 echo "Customer ID should not be empty";
+	 die();
+ }
+?>
